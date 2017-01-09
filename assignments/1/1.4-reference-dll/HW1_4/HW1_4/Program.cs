db9a677c3dde0eb829e6037a5c8cf2ad7dc04932
soakes84 <spencer.oakes84@gmail.com>
2017-01-09 15:06:38 -0500

    Hopefully added HW1_4 folder

---
+++ assignments/1/1.4-reference-dll/HW1_4/HW1_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reference.Synthesizer sythesizer = new Reference.Synthesizer();
+            sythesizer.SayHello();
+        }
+    }
+
+}
