d657c57d4880bae00500f349bd98ae44e48a69a7
Your Name <jeffheffner@gmail.com>
2017-01-09 14:39:41 -0500

    This folder contains my console application

---
+++ assignments/1/1.4-reference-dll/Homework1_4/Homework1_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_4
+{
+    class Homework1_4
+    {
+        static void Main(string[] args)
+        {
+            Reference.Synthesizer synthesizer = new Reference.Synthesizer();
+            synthesizer.SayHello();
+        }
+    }
+}
