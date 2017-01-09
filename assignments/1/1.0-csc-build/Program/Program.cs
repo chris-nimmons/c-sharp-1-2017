de03c5f76b003910fddb6593c6e045429244d92d
Your Name <jeffheffner@gmail.com>
2017-01-09 14:14:20 -0500

    Adding my Program project file

---
+++ assignments/1/1.0-csc-build/Program/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using Application;
+
+namespace Application
+{
+    public class Program
+    {
+public static void Main(string[] args)
+{
+    Console.WriteLine("Octo Cats unite");
+    Console.ReadLine();
+}
+
+    }
+}
