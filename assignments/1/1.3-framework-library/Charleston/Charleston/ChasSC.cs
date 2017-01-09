af783949dd1c3890e1cedbae990047f4fba69a33
Mackenzie Lewerke <mackenzielewerke@gmail.com>
2017-01-09 14:52:49 -0500

    New ChasSC.cs for 1.3

---
+++ assignments/1/1.3-framework-library/Charleston/Charleston/ChasSC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charleston
+{
+    public class ChasSC
+    {
+        public string Welcome(string input)
+        {
+            return "The Holy City";
+        }
+    }
+}
