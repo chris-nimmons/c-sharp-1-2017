4fb2f8c4862a0224a35d3fc2115b11c54d59273e
soakes84 <spencer.oakes84@gmail.com>
2017-01-09 14:30:39 -0500

    Hopefully added the whole folder

---
+++ assignments/1/1.3-framework-library/HW 1.3/HW 1.3/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_1._3
+{
+    public class HW_1_3
+    {
+    }
+}
