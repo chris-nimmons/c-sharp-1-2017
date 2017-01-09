cd681539536c406cd8b42e1ed62c9bab02902e01
JeffWeinreich <jeffweinreich@gmail.com>
2017-01-09 15:07:13 -0500

    Added Consoledll

---
+++ assignments/1/1.4-reference-dll/Consoledll/Consoledll/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoledll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reference.Synthesizer synthesizer = new Reference.Synthesizer();
+           synthesizer.SayHello();
+                
+        }
+    }
+}
