38641e1e43366917149b43786ff1b106ccd439cd
Your Name <jeffheffner@gmail.com>
2017-01-09 14:10:16 -0500

    My project folder for 1.2

---
+++ assignments/1/1.2-dot-net-framework/Homework1.2/Homework1.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_2
+{
+    class Homework1_2
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
