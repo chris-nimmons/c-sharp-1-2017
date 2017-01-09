2cdd8e9e0305bce9131b5756d562fbcb5ff6b049
Kyle Tankersley <Kyle Tankersley>
2017-01-09 14:10:12 -0500

    git remove assignment

---
+++ programs/Git_remove.cs
@@ -0,0 +1,18 @@
+using System;
+using Application;
+
+namespace Application
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Did you hear the one about the man who lost his whole left side?");
+			Console.ReadLine();
+			
+			Console.WriteLine("I hear he's ALL RIGHT now.");
+			Console.ReadLine();
+		}
+	}
+	
+}
