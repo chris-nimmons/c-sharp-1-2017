24554b1094541204c02ea040cf039ce31e462a9d
Alex Marcondes <marcondag@gmail.com>
2017-01-09 13:36:43 -0500

    Solution to 1.3-framework-library

---
+++ assignments/1/1.3-framework-library/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class SolutionForOnePointThree
+    {
+        public static void PrintSolution()
+        {
+            Console.WriteLine("This is a method (PrintSolution()) inside of the SolutionForOnePointThree class");
+            Console.WriteLine("all contained inside of the ClassLibrary1 library made as a solution for");
+            Console.WriteLine("assignment 1.3-framework-library.");
+        }
+    }
+}
