9209a70859bc1a4a15d692cd227aabeb7cc2b03f
Kyle Tankersley <Kyle Tankersley>
2017-01-10 16:16:58 -0500

    Assingment 2 1/10/2017

---
+++ assignments/2/2.0-fox-translator/WhatDoesTheFoxSay/FoxLibrary/FoxSay.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Fox
+{
+    public class FoxSay
+    {
+        public void FoxTranslate()
+        {
+            string fox = "wa pow ring ding tchoff pa joff ho hatee tchoffo dingeringeding";
+
+            string[] sentenceArray = fox.Split(' ');
+
+            foreach (string word in sentenceArray)
+            {
+
+                if (word == "wa")
+                {
+                    Console.WriteLine("you");
+                    Console.ReadLine();
+                }
+                else if (word == "pow")
+                {
+                    Console.WriteLine("owe");
+                    Console.ReadLine();
+                }
+                else if (word == "tchoffo")
+                {
+                    Console.WriteLine("like");
+                    Console.ReadLine();
+                }
+                else if (word == "pa")
+                {
+                    Console.WriteLine("acorn");
+                    Console.ReadLine();
+                }
+                else if (word == "joff")
+                {
+                    Console.WriteLine("and");
+                    Console.ReadLine();
+                }
+                else if (word == "ho")
+                {
+                    Console.WriteLine("I");
+                    Console.ReadLine();
+                }
+                else if (word == "ring")
+                {
+                    Console.WriteLine("me");
+                    Console.ReadLine();
+                }
+                else if (word == "hatee")
+                {
+                    Console.WriteLine("don't");
+                    Console.ReadLine();
+                }
+                else if (word == "ding")
+                {
+                    Console.WriteLine("a");
+                    Console.ReadLine();
+                }
+                else if (word == "dingeringeding")
+                {
+                    Console.WriteLine("squirrels");
+                    Console.ReadLine();
+                }
+                else if (word == "tchoff")
+                {
+                    Console.WriteLine("new");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("wololo");
+                    Console.ReadLine();
+                }                
+
+            }
+        }
+    }
+}
---
+++ assignments/2/2.0-fox-translator/WhatDoesTheFoxSay/WhatDoesTheFoxSay/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Fox;
+
+
+/*
+ Using for, foreach, split and if statements translate what the fox says.  
+
+"wa pow ring ding tchoff pa joff ho hatee tchoffo dingeringeding"
+
+Legend:
+
+like - tchoffo
+owe - pow
+acorn - pa
+and - joff
+I - ho
+me - Ring 
+dont - hatee
+a - ding
+squirrels - dingeringeding
+you - wa
+new - tchoff
+*/
+
+
+namespace WhatDoesTheFoxSay
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FoxSay foxsay = new FoxSay();
+            foxsay.FoxTranslate();
+
+        }
+    }
+}
