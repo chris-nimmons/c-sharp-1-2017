6c062d5beae4ffd6ab569f66fe1ce3efef732583
Alex Marcondes <marcondag@gmail.com>
2017-01-10 15:46:37 -0500

    Added method using if/else

--- assignments/2/2.0-fox-translator/FoxTranslator/MainMethod/Program.cs
+++ assignments/2/2.0-fox-translator/FoxTranslator/MainMethod/Program.cs
@@ -11,10 +11,8 @@ namespace FoxTranslator
         static void Main(string[] args)
         {
             string foxSpeak = "wa pow ring ding tchoff pa joff ho hatee tchoffo dingeringeding";
-            Console.WriteLine("I'm going to translate:\n{0}\nfrom foxspeak to human speak.", foxSpeak);
             Translator translator = new FoxTranslator.Translator();
-            string humanSpeak = translator.Translate(foxSpeak);
-            Console.WriteLine("\n" + humanSpeak);
+            Console.WriteLine(translator.AlternativeTranslate(foxSpeak));
             Console.ReadLine();
         }
     }
--- assignments/2/2.0-fox-translator/FoxTranslator/MainMethod/Translator.cs
+++ assignments/2/2.0-fox-translator/FoxTranslator/MainMethod/Translator.cs
@@ -19,6 +19,61 @@ namespace FoxTranslator
                 { "like", "owe", "acorn", "and", "I", "me", "dont", "a", "squirrels", "you", "new" };
         }
 
+        public string AlternativeTranslate(string foxSpeak)
+        {
+            string[] foxSpeakSplitted = foxSpeak.Split();
+            string humanSpeak = "";
+            foreach (string word in foxSpeakSplitted)
+            {
+                if (word == "tchoffo")
+                {
+                    humanSpeak += "like ";
+                }
+                else if (word == "pow")
+                {
+                    humanSpeak += "owe ";
+                }
+                else if (word == "pa")
+                {
+                    humanSpeak += "acorn ";
+                }
+                else if (word == "joff")
+                {
+                    humanSpeak += "and ";
+                }
+                else if (word == "ho")
+                {
+                    humanSpeak += "I ";
+                }
+                else if (word == "ring")
+                {
+                    humanSpeak += "me ";
+                }
+                else if (word == "hatee")
+                {
+                    humanSpeak += "dont ";
+                }
+                else if (word == "ding")
+                {
+                    humanSpeak += "a ";
+                }
+                else if (word == "dingeringeding")
+                {
+                    humanSpeak += "squirrels ";
+                }
+                else if (word == "wa")
+                {
+                    humanSpeak += "you ";
+                }
+                else if (word == "tchoff")
+                {
+                    humanSpeak += "new ";
+                }
+            }
+
+            return humanSpeak;
+        }
+
         public string Translate(string foxSpeak)
         {
             //Split sentence
