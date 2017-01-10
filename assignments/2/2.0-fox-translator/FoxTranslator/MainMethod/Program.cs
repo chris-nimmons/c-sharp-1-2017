08df196da63783e1c444aa3c23321480703ce603
Alex Marcondes <marcondag@gmail.com>
2017-01-10 14:43:03 -0500

    Solution to 2.0-fox-translator

---
+++ assignments/2/2.0-fox-translator/FoxTranslator/MainMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string foxSpeak = "wa pow ring ding tchoff pa joff ho hatee tchoffo dingeringeding";
+            Console.WriteLine("I'm going to translate:\n{0}\nfrom foxspeak to human speak.", foxSpeak);
+            Translator translator = new FoxTranslator.Translator();
+            string humanSpeak = translator.Translate(foxSpeak);
+            Console.WriteLine(humanSpeak);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assignments/2/2.0-fox-translator/FoxTranslator/MainMethod/Translator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxTranslator
+{
+    class Translator
+    {
+        private string[] foxWords;
+        private string[] humanWords;
+
+        public Translator()
+        {
+            foxWords = new string[11] 
+                { "tchoffo", "pow", "pa", "joff", "ho", "ring", "hatee", "ding", "dingeringeding", "wa", "tchoff" };
+            humanWords = new string[11]
+                { "like", "owe", "acorn", "and", "I", "me", "dont", "a", "squirrels", "you", "new" };
+        }
+
+        public string Translate(string foxSpeak)
+        {
+            //Split sentence
+            //Create new string[] same size as Splitsentence
+            //Iterate each word and find foxWords index
+            //Replace humanSpeak index w/ humanword
+            string[] foxSpeakSplitted = foxSpeak.Split();
+            string[] humanSpeak = new string[foxSpeak.Length];
+            for (int sentenceIndex = 0; sentenceIndex < foxSpeakSplitted.Length; sentenceIndex++)
+            {
+                for (int translatorIndex = 0; translatorIndex < foxWords.Length; translatorIndex++)
+                {
+                    if (foxSpeakSplitted[sentenceIndex] == foxWords[translatorIndex])
+                    {
+                        humanSpeak[sentenceIndex] = humanWords[translatorIndex];
+                    }
+                }
+            }
+
+            string translatedSentence = "";
+
+            foreach (string word in humanSpeak)
+            {
+                translatedSentence += word + " ";
+            }
+
+            return translatedSentence;
+        }
+    }
+}
