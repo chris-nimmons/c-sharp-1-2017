9d70c40cdb92b9930c5449c76ff4509621b24629
soakes84 <spencer.oakes84@gmail.com>
2017-01-10 16:30:33 -0500

    Hopefully added whole fox translator folder

---
+++ assignments/2/2.0-fox-translator/FoxTranslator/FoxTranslator/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoxTranslator.Translator;
+
+
+namespace FoxTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "wa pow ring ding tchoff pa joff ho hatee tchoffo dingeringeding";
+            Translators translate = new Translators();
+            string translation = translate.Translate(sentence);
+            
+
+
+            
+            //string[] words = sentence.Split(' ');
+            //string translatedSentence = string.Empty;
+
+            //foreach (string word in words)
+            //{
+            //    if (word == "wa")
+            //    {
+            //        translatedSentence += "you ";
+            //    }
+            //    else if (word == "pow")
+            //    {
+            //        translatedSentence += "owe ";
+            //    }
+            //    else if (word == "ring")
+            //    {
+            //        translatedSentence += "me ";
+            //    }
+            //    else if (word == "ding")
+            //    {
+            //        translatedSentence += "a ";
+            //    }
+            //    else if (word == "tchoff")
+            //    {
+            //        translatedSentence += "new ";
+            //    }
+            //    else if (word == "pa")
+            //    {
+            //        translatedSentence += "acorn ";
+            //    }
+            //    else if (word == "joff")
+            //    { 
+            //        translatedSentence += "and ";
+            //    }
+            //    else if (word == "ho")
+            //    {
+            //        translatedSentence += "I ";
+            //    }
+            //    else if (word == "hatee")
+            //    {
+            //        translatedSentence += "don't ";
+            //    }
+            //    else if (word == "tchoffo")
+            //    {
+            //        translatedSentence += "like ";
+            //    }
+            //    else if (word == "dingeringeding")
+            //    {
+            //        translatedSentence += "squirrels!";
+            //    }
+
+            //}
+            Console.WriteLine(translation);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ assignments/2/2.0-fox-translator/FoxTranslator/FoxTranslator/Translator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxTranslator.Translator
+{
+    public class Translators
+    {
+        public string Translate(string sentence)
+        {
+       
+            string[] words = sentence.Split(' ');
+            string translatedSentence = string.Empty;
+
+            foreach (string word in words)
+            {
+                if (word == "wa")
+                {
+                    translatedSentence += "you ";
+                }
+                else if (word == "pow")
+                {
+                    translatedSentence += "owe ";
+                }
+                else if (word == "ring")
+                {
+                    translatedSentence += "me ";
+                }
+                else if (word == "ding")
+                {
+                    translatedSentence += "a ";
+                }
+                else if (word == "tchoff")
+                {
+                    translatedSentence += "new ";
+                }
+                else if (word == "pa")
+                {
+                    translatedSentence += "acorn ";
+                }
+                else if (word == "joff")
+                {
+                    translatedSentence += "and ";
+                }
+                else if (word == "ho")
+                {
+                    translatedSentence += "I ";
+                }
+                else if (word == "hatee")
+                {
+                    translatedSentence += "don't ";
+                }
+                else if (word == "tchoffo")
+                {
+                    translatedSentence += "like ";
+                }
+                else if (word == "dingeringeding")
+                {
+                    translatedSentence += "squirrels!";
+                }
+
+            }
+            return translatedSentence;
+        }
+    }
+}
