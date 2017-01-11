64835f02123323fd29712b74aeb70ac67099ed0a
Alex Marcondes <marcondag@gmail.com>
2017-01-11 13:29:02 -0500

    Solution to 3.0-staircase

---
+++ assignments/3/3.0-staircase/StairCase/StairCase/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StairCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintStairs(53);
+            Console.ReadLine();
+        }
+
+        static void PrintStairs(int totalSteps)
+        {
+            StairBuilder stairBuilder = new StairCase.StairBuilder();
+            for (int i = 1; i <= totalSteps; i++)
+            {
+                Console.WriteLine(stairBuilder.NextStep(i, totalSteps));
+            }
+        }
+    }
+}
---
+++ assignments/3/3.0-staircase/StairCase/StairCase/StairBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StairCase
+{
+    class StairBuilder
+    {
+        public string NextStep(int stepNumber, int totalSteps)
+        {
+            //blanks = totalSteps - stepNumber
+            //"x"s = stepNumber
+            
+            int blanks = totalSteps - stepNumber;
+
+            return Blanks(blanks) + Exes(stepNumber);
+
+        }
+
+        public string Blanks(int blanks)
+        {
+            StringBuilder airSpace = new StringBuilder();
+            
+            for (int i = 0; i < blanks; i++)
+            {
+                airSpace.Append(" ");
+            }
+            
+            return airSpace.ToString();
+        }
+
+        public string Exes(int stepNumber)
+        {
+            StringBuilder exes = new StringBuilder();
+
+            for (int i = 0; i < stepNumber; i++)
+            {
+                exes.Append("x");
+            }
+
+            return exes.ToString();
+        }
+    }
+}
