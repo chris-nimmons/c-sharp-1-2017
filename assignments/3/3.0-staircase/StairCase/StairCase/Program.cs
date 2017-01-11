07812b46627d0c2da0496d32f0fd0f2c653ca751
Alex Marcondes <marcondag@gmail.com>
2017-01-11 14:43:43 -0500

    Made the solution simpler

--- assignments/3/3.0-staircase/StairCase/StairCase/Program.cs
+++ assignments/3/3.0-staircase/StairCase/StairCase/Program.cs
@@ -14,12 +14,29 @@ namespace StairCase
             Console.ReadLine();
         }
 
-        static void PrintStairs(int totalSteps)
+        static void PrintWithStairBuilder(int totalSteps)
         {
             StairBuilder stairBuilder = new StairCase.StairBuilder();
             for (int i = 1; i <= totalSteps; i++)
             {
                 Console.WriteLine(stairBuilder.NextStep(i, totalSteps));
+            }
+        }
+
+        static void PrintStairs(int totalSteps)
+        {
+            for (int steps = 1; steps <= totalSteps; steps++)
+            {
+                for (int blanks = steps; blanks <= totalSteps - 1; blanks++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int exes = steps; exes > 0; exes--)
+                {
+                    Console.Write("x");
+                }
+                Console.WriteLine();
             }
         }
     }
