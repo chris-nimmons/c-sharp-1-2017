f635760cd375b256737a429646f1c92a535088f0
soakes84 <spencer.oakes84@gmail.com>
2017-01-11 15:36:23 -0500

    Hopefully THIS time added the whole folder

---
+++ assignments/3/3.0-staircase/StairCase/StairCase/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StairCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stairCase = "";
+            char addX = 'x';
+            string space = " ";
+            int numSteps = 6;
+
+            for (int x = 0; x < numSteps; x++)
+            {
+                for (int y = x; y < numSteps - 1; y++)
+                {
+                    
+                    Console.Write(space);
+                }
+
+                stairCase = stairCase + addX;            
+                Console.WriteLine(stairCase);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
