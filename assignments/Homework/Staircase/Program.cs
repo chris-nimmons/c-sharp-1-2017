9a1c856de6ca9bedc35daa62c130e889a3f602f2
Mackenzie Lewerke <mackenzielewerke@gmail.com>
2017-01-11 16:45:53 -0500

    Stairway to Hell... just kidding

---
+++ assignments/Homework/Staircase/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 6;
+            int cols = 6;
+            for (int row = 0; row < rows; row++)
+            {
+                int spaces = cols - (row + 1);
+
+                for (int col = 0; col < cols; col++)
+                {
+                    if (col >= spaces)
+                    {
+                        Console.Write("x");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
