96e0b3006341274dae7b5921ad07b179cbae7a7c
Colby-Burke <burkecl@g.cofc.edu>
2017-01-17 14:14:56 -0500

    Room Render

--- assignments/4/RenderFromScratch/RenderFromScratch/Program.cs
+++ assignments/4/RenderFromScratch/RenderFromScratch/Program.cs
@@ -13,22 +13,36 @@ namespace RenderFromScratch
             Program program = new Program();
             program.Start();
             Console.ReadLine();
+            program.Start();
+            Console.ReadLine();
+            program.Start();
+            Console.ReadLine();
+            program.Start();
+            Console.Write("You're done!!! Press enter!");
 
 
         }
+
         public void Start()
         {
             List<IRenderable> renderables = new List<IRenderable>();
 
             Console.WriteLine("Enter desired Length of room (0-100): ");
-            int roomLength = int.Parse(Console.ReadLine());
+            int Length = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Enter desired Width of room (0-100): ");
-            int roomWidth = int.Parse(Console.ReadLine());
+            int Width = int.Parse(Console.ReadLine());
+
             Console.ReadLine();
 
             Random random = new Random();
             int randomNumber = random.Next(0, 100);
+
+            Room room = new Room();
+            room.positionX = randomNumber;
+            room.positionY = randomNumber;
+            room.Length = Length;
+            room.Width = Width;
 
             Table table = new Table();
             table.X = randomNumber;
@@ -42,10 +56,6 @@ namespace RenderFromScratch
             chair.X = randomNumber;
             chair.Y = randomNumber;
 
-            Room room = new Room();
-            room.Length = roomLength;
-            room.Width = roomWidth;
-
             renderables.Add(table);
             renderables.Add(table2);
             renderables.Add(chair);
@@ -54,39 +64,7 @@ namespace RenderFromScratch
             Renderer render = new Renderer();
             render.Render(renderables);
 
-            if (roomLength > 0 || roomWidth > 0)
-            {
-                Console.WriteLine();
-                for (int y = 0; y < roomLength; y++)
-                {
-                    for (int x = 0; x < roomWidth; x++)
-                    {
-                        if (y == 0)
-                        {
-                            Console.Write("*");
-                        }
-                        else if (y == roomLength - 1)
-                        {
-                            Console.Write("*");
-                        }
-                        else if (x == 0)
-                        {
-                            Console.Write("*");
-                        }
-                        else if(x == roomWidth - 1)
-                        {
-                            Console.Write("*");
-                        }
-                        else
-                        {
-                            Console.Write(" ");
-                        }
-                    }
-
-                    Console.WriteLine();
-                }
-            }
-    }
+        }
 
 
         public interface IRenderable
@@ -119,14 +97,48 @@ namespace RenderFromScratch
         {
             public int Length { get; set; }
             public int Width { get; set; }
+            public int positionX { get; set; }
+            public int positionY { get; set; }
 
             public void Render()
             {
-                Console.SetCursorPosition(Length, Width);
-                Console.Write('*');
+                Console.SetCursorPosition(positionX, positionY);
+                    if (positionX > 0 && positionY > 0)
+                    {
+                        Console.WriteLine();
+                        for (int y = 0; y < Length; y++)
+                        {
+                            for (int x = 0; x < Width; x++)
+                            {
+                                if (y == 0)
+                                {
+                                    Console.Write("*");
+                                }
+                                else if (y == Length - 1)
+                                {
+                                    Console.Write("*");
+                                }
+                                else if (x == 0)
+                                {
+                                    Console.Write("*");
+                                }
+                                else if (x == Width - 1)
+                                {
+                                    Console.Write("*");
+                                }
+                                else
+                                {
+                                    Console.Write(" ");
+                                }
+                            }
+
+                            Console.WriteLine();
+                        }
+                    }
+               }
             }
-        }
-        public class Renderer
+
+            public class Renderer
         {
             public void Render(List<IRenderable> renderables)
             {
@@ -139,6 +151,6 @@ namespace RenderFromScratch
         }
 
     }
-
 }
+
 
