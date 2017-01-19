61c8346ac61c02f476e126af95dfb6765fcbd021
Colby-Burke <burkecl@g.cofc.edu>
2017-01-19 14:10:25 -0500

    project 3

---
+++ assignments/3/StaircaseProject/StaircaseProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x <= 6; x++)
+            {
+                for (int y = 1; y <= (6 - x); y++)
+                {
+                    Console.Write(" ");
+                }
+                for (int z = 1; z <= x; z++)
+                {
+                    Console.Write("x");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
