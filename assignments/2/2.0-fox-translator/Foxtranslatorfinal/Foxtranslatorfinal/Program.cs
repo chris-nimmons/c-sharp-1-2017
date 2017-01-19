d1b745b592a703dda8a5b5f4e88f04b75cac0b77
JeffWeinreich <jeffweinreich@gmail.com>
2017-01-19 08:58:14 -0500

    Added Foxtranslatorfinal

---
+++ assignments/2/2.0-fox-translator/Foxtranslatorfinal/Foxtranslatorfinal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foxtranslatorfinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "wa pow ring ding tchoff pa joff ho hatee tchoffo dingeringeding.";
+
+
+
+            Translators translate = new Translators();
+            string translation = translate.Translate(sentence);
+            
+            Console.WriteLine(translation);
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
