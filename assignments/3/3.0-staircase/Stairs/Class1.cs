abbe327fdde41b6d4484ee5f91e857cfb15e1d74
Your Name <jeffheffner@gmail.com>
2017-01-19 15:21:31 -0500

    adding stairs once again

---
+++ assignments/3/3.0-staircase/Stairs/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int rows = 6;
+            int columns = 7;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < columns; col++)
+                {
+                    int spaces = rows - row;
+                    if (col + 1 > spaces)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
