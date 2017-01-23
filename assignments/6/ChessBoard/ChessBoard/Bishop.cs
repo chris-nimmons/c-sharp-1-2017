8349842d980e0ce79870aafd840cf5636c9cda97
Colby-Burke <burkecl@g.cofc.edu>
2017-01-22 21:10:49 -0500

    Project 6

---
+++ assignments/6/ChessBoard/ChessBoard/Bishop.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoard
+{
+    class Bishop : Piece
+    {
+        public void BishopPiece()
+        {
+            int Width = 5;
+            int Height = 5;
+
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    if (y == 0)
+                    {
+                        Console.SetCursorPosition(10, 8);
+                        Console.Write("X   X");
+                    }
+                    else if (y == 1)
+                    {
+                        Console.SetCursorPosition(10, 9);
+                        Console.Write(" X X");
+                    }
+                    else if (y == 2)
+                    {
+                        Console.SetCursorPosition(10, 10);
+                        Console.Write("  O");
+                    }
+                    else if (y == 3)
+                    {
+                        Console.SetCursorPosition(10, 11);
+                        Console.Write(" X X");
+                    }
+                    else if (y == 4)
+                    {
+                        Console.SetCursorPosition(10, 12);
+                        Console.Write("X   X");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assignments/6/ChessBoard/ChessBoard/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IRenderable> renderables = new List<IRenderable>();
+
+            var chessset = new ChessSet();
+            var pawn = new Pawn();
+
+            renderables.Add(chessset);
+            renderables.Add(pawn);
+            Console.ReadLine();
+        }
+
+    
+
+        }
+
+        public interface IRenderable
+        {
+            void Render();
+        }
+        public class Renderer
+        {
+            public void Render(List<IRenderable> renderables)
+            {
+                foreach (IRenderable renderable in renderables)
+                {
+                    renderable.Render();
+                }
+            }
+        }
+}
