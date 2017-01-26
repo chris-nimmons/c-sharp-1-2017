a00b8c7f973027b63ec56a844ae6c846a867d19f
Alex Marcondes <marcondag@gmail.com>
2017-01-26 13:37:17 -0500

    Volume properties set private fields using absolute value

--- assignments/7/7.0-tdd/TestDrivenDesignLecture/TestDrivenDesignLecture/Volume.cs
+++ assignments/7/7.0-tdd/TestDrivenDesignLecture/TestDrivenDesignLecture/Volume.cs
@@ -8,16 +8,20 @@ namespace TestDrivenDesignLecture
 {
     public class Volume
     {
-        public float Length { get; set; }
-        public float Width { get; set; }
-        public float Height { get; set; }
+        private float _length;
+        private float _width;
+        private float _height;
+
+        public float Length { get { return _length; } set { _length = Math.Abs(value); } }
+        public float Width { get { return _width; } set { _width = Math.Abs(value); } }
+        public float Height { get { return _height; } set { _height = Math.Abs(value); } }
         public float Value { get { return Length * Width * Height; } }
 
         public Volume(float length, float width, float height)
         {
-            Length = Math.Abs(length);
-            Width = Math.Abs(width);
-            Height = Math.Abs(height);
+            Length = length;
+            Width = width;
+            Height = height;
         }
     }
 }
