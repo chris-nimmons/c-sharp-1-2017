9c25720ad1a445ba6703b4863dbc418b7f691735
Colby-Burke <burkecl@g.cofc.edu>
2017-01-27 02:49:13 -0500

    7

---
+++ assignments/New folder/TestProject7/TestProject7/Volume.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrivenDesignLecture
+{
+    public class Volume
+    {
+        public float Length { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public float Value
+        {
+            get { return Length * Width * Height; }
+
+        }
+
+        public Volume(float length, float width, float height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+
+    }
+}
