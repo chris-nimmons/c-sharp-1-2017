84792630a2a3afc2f00e4201ef09333df64216c9
Colby-Burke <burkecl@g.cofc.edu>
2017-01-29 11:51:55 -0500

    Chess game

--- assignments/6/Project6/Project6/Board.cs
+++ assignments/6/Project6/Project6/Board.cs
@@ -8,20 +8,86 @@ namespace Project6
 {
     class Board
     {
+
+        private const int boardWidth = 8;
+        private const int boardLength = 8;
+
+        /// <summary>
+        /// Chess board.
+        /// </summary>
+        private Piece[,] chessBoard = new Piece[8, 8];
+
         public Board()
         {
-            int width = 8;
-            int length = 8;
-            for(int i = 0; i <width-1; i++)
+
+            PreLoadChessBoard();
+
+            int width = boardWidth;
+            int length = boardLength;
+
+            DisplayChessBoardWithPieces(width, length);
+
+        }
+
+        private static void DisplayChessBoardWithPieces(int width, int length)
+        {
+            for (int i = 0; i < width ; i++)
             {
-                for(int j = 0; j < length-1; j++)
+
+                for (int j = 0; j < length; j++)
                 {
                     Console.SetCursorPosition(i, j);
                     Console.Write("X");
                 }
-                Console.Write(" ");
+
             }
         }
 
+        /// <summary>
+        /// Sets up chess board with the initial position for the black and white pieces.
+        /// </summary>
+        private void PreLoadChessBoard()
+        {
+
+            SetPiece(new King() { X = 3, Y = 0 });
+            SetPiece(new Queen() { X = 4, Y = 0 });
+            SetPiece(new Bishop() { X = 2, Y = 0 });
+            SetPiece(new Bishop() { X = 5, Y = 0 });
+            SetPiece(new Knight() { X = 1, Y = 0 });
+            SetPiece(new Castle() { X = 0, Y = 0 });
+            SetPiece(new Knight() { X = 6, Y = 0 });
+            SetPiece(new Castle() { X = 7, Y = 0 });
+            SetPiece(new Pawn() { X = 0, Y = 1 });
+            SetPiece(new Pawn() { X = 1, Y = 1 });
+            SetPiece(new Pawn() { X = 2, Y = 1 });
+            SetPiece(new Pawn() { X = 3, Y = 1 });
+            SetPiece(new Pawn() { X = 4, Y = 1 });
+            SetPiece(new Pawn() { X = 5, Y = 1 });
+            SetPiece(new Pawn() { X = 6, Y = 1 });
+            SetPiece(new Pawn() { X = 7, Y = 1 });
+
+            SetPiece(new King() { X = 4, Y = 7 });
+            SetPiece(new Queen() { X = 3, Y = 7 });
+            SetPiece(new Bishop() { X = 2, Y = 7 });
+            SetPiece(new Bishop() { X = 5, Y = 7 });
+            SetPiece(new Knight() { X = 1, Y = 7 });
+            SetPiece(new Castle() { X = 0, Y = 7 });
+            SetPiece(new Knight() { X = 6, Y = 7 });
+            SetPiece(new Castle() { X = 7, Y = 7 });
+            SetPiece(new Pawn() { X = 0, Y = 6 });
+            SetPiece(new Pawn() { X = 1, Y = 6 });
+            SetPiece(new Pawn() { X = 2, Y = 6 });
+            SetPiece(new Pawn() { X = 3, Y = 6 });
+            SetPiece(new Pawn() { X = 4, Y = 6 });
+            SetPiece(new Pawn() { X = 5, Y = 6 });
+            SetPiece(new Pawn() { X = 6, Y = 6 });
+            SetPiece(new Pawn() { X = 7, Y = 6 });
+
+        }
+
+        private void SetPiece(Piece piece)
+        {
+            chessBoard[piece.X, piece.Y] = piece;
+        }
     }
 }
--- assignments/6/Project6/Project6/Program.cs
+++ assignments/6/Project6/Project6/Program.cs
@@ -8,12 +8,19 @@ namespace Project6
 {
     class Program
     {
+
         static void Main(string[] args)
         {
 
             Renderer renderer = new Renderer();
             Cursor cursor = new Cursor();
+
+            Board board = new Board();
+
+
+
             var pieces = new List<Piece>();
+
             var king = new King() { X = 3, Y = 0 };
             var queen = new Queen() { X = 4, Y = 0 };
             var bishop = new Bishop() { X = 2, Y = 0 };
@@ -21,10 +28,10 @@ namespace Project6
             var knight = new Knight() { X = 1, Y = 0 };
             var castle = new Castle() { X = 0, Y = 0 };
             var knight2 = new Knight() { X = 6, Y = 0 };
-            var castle2 = new Castle() { X = 7, Y= 0};
+            var castle2 = new Castle() { X = 7, Y = 0 };
 
             var kingBlack = new King() { X = 4, Y = 7 };
-            var queenBlack = new Queen() { X = 3, Y = 7};
+            var queenBlack = new Queen() { X = 3, Y = 7 };
             var bishopBlack = new Bishop() { X = 2, Y = 7 };
             var bishop2Black = new Bishop() { X = 5, Y = 7 };
             var knightBlack = new Knight() { X = 1, Y = 7 };
@@ -42,6 +49,8 @@ namespace Project6
                 pawn = new Pawn() { Y = 6, X = x };
                 pieces.Add(pawn);
             }
+
+
 
             pieces.Add(castle2Black);
             pieces.Add(knightBlack);
@@ -72,97 +81,96 @@ namespace Project6
 
 
             Console.SetCursorPosition(cursor.X, cursor.Y);
-            bool running = true;
-            while (running)
-            {
-                var info = Console.ReadKey(true);
-                switch (info.Key)
+                bool running = true;
+                while (running)
                 {
-                    case ConsoleKey.Escape:
-                        running = false;
-                        break;
-                    case ConsoleKey.UpArrow:
-                        cursor.Y--;
-                        break;
-                    case ConsoleKey.DownArrow:
-                        cursor.Y++;
-                        break;
-                    case ConsoleKey.LeftArrow:
-                        cursor.X--;
-                        break;
-                    case ConsoleKey.RightArrow:
-                        cursor.X++;
-                        break;
-                    case ConsoleKey.Enter:
-                        Piece highlighted = null;
-                        foreach (var piece in pieces)
-                        {
-                            if (cursor.X == piece.X && cursor.Y == piece.Y)
+                    var info = Console.ReadKey(true);
+                    switch (info.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            running = false;
+                            break;
+                        case ConsoleKey.UpArrow:
+                            cursor.Y--;
+                            break;
+                        case ConsoleKey.DownArrow:
+                            cursor.Y++;
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            cursor.X--;
+                            break;
+                        case ConsoleKey.RightArrow:
+                            cursor.X++;
+                            break;
+                        case ConsoleKey.Enter:
+                            Piece highlighted = null;
+                            foreach (var piece in pieces)
                             {
-                                highlighted = piece;
-                            }
-                        }
-                        if (highlighted != null)
-                        {
-                            if (selection != null)
-                            {
-                                if (highlighted == selection)
+                                if (cursor.X == piece.X && cursor.Y == piece.Y)
                                 {
-                                    Console.Write(selection.Letter);
-                                    selection = null;
+                                    highlighted = piece;
+                                }
+                            }
+                            if (highlighted != null)
+                            {
+                                if (selection != null)
+                                {
+                                    if (highlighted == selection)
+                                    {
+                                        Console.Write(selection.Letter);
+                                        selection = null;
+                                    }
+                                    else
+                                    {
+
+                                    }
                                 }
                                 else
                                 {
-
+                                    selection = highlighted;
+                                    Console.Write("X");
                                 }
                             }
                             else
                             {
-                                selection = highlighted;
-                                Console.Write(" ");
-                            }
-                        }
-                        else
-                        {
-                            if (selection != null)
-                            {
-                                Console.Write(selection.Letter);
-                                selection.X = cursor.X;
-                                selection.Y = cursor.Y;
+                                if (selection != null)
+                                {
+                                    Console.Write(selection.Letter);
+                                    selection.X = cursor.X;
+                                    selection.Y = cursor.Y;
 
-                                selection = null;
+                                    selection = null;
+                                }
                             }
-                        }
-                        break;
-                }
-                if (cursor.X < 0 || cursor.Y < 0)
-                {
-                    cursor.X = 0;
-                    cursor.Y = 0;
-                }
-                if (cursor.X > 7 || cursor.Y < 0)
-                {
-                    cursor.X = 0;
+                            break;
+                    }
+                    if (cursor.X < 0 || cursor.Y < 0)
+                    {
+                        cursor.X = 0;
+                        cursor.Y = 0;
+                    }
+                    if (cursor.X > 7 || cursor.Y < 0)
+                    {
+                        cursor.X = 0;
 
+                    }
+                    if (cursor.X > 7)
+                    {
+                        cursor.X = 0;
+                    }
+                    if (cursor.X < 0 || cursor.Y > 7)
+                    {
+                        cursor.Y = 0;
+                    }
+                    Console.SetCursorPosition(cursor.X, cursor.Y);
                 }
-                if (cursor.X > 7)
-                {
-                    cursor.X = 0;
-                }
-                if (cursor.X < 0 || cursor.Y > 7)
-                {
-                    cursor.Y = 0;
-                }
-                Console.SetCursorPosition(cursor.X, cursor.Y);
             }
+
         }
+        public class Cursor
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
 
+        }
     }
-    public class Cursor
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-
-    }
-}
-
