71b23c6ade5d4afd476d6efc5947888018e23ea1
ptank86 <kptankersley@gmail.com>
2017-02-13 07:29:27 -0500

    Add files via upload

---
+++ assignments/9/9.0-razor/CoreShop/src/CoreShop/Models/ShopContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreShop.Models
+{
+    public class ShopContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsbuilder)
+        {
+            optionsbuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=CoreShop;Trusted_Connection=True;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public ShopContext() 
+        {
+
+
+            DbContextOptionsBuilder optionsbuilder = new DbContextOptionsBuilder();
+            OnConfiguring(optionsbuilder);
+
+
+        }
+
+
+    }
+}
