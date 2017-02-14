3a98127fe24b01dab99804655ab2f05f600ccb7a
soakes84 <spencer.oakes84@gmail.com>
2017-02-14 16:24:01 -0500

    Most updated Shop- has the 1st register/login methods we learned

--- assignments/9/9.0-razor/Shop/Shop.Web/Global.asax.cs
+++ assignments/9/9.0-razor/Shop/Shop.Web/Global.asax.cs
@@ -1,7 +1,10 @@
-﻿using Shop.Models;
+﻿using Newtonsoft.Json;
+using Shop.Models;
+using Shop.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -12,13 +15,13 @@ namespace Shop.Web
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
-        
+
         public static Cart Cart;
 
         public WebApiApplication()
         {
             this.AuthenticateRequest += WebApiApplication_AuthenticateRequest;
-           
+
         }
 
         protected void Application_Start()
@@ -34,6 +37,22 @@ namespace Shop.Web
 
         private void WebApiApplication_AuthenticateRequest(object sender, EventArgs e)
         {
+
+            var cookieUser = HttpContext.Current.Request.Cookies["authentication"];
+            var authentication = new AuthenticationContext();
+
+            if (cookieUser == null)
+            {
+                var account = new Account() { Signature = Guid.NewGuid() };
+
+            }
+            else
+            {
+                var input = cookieUser.Value;
+                UserModel user = JsonConvert.DeserializeObject<UserModel>(input);
+                HttpContext.Current.User = new GenericPrincipal(new GenericIdentity(user.Name), user.Roles.ToArray());
+            }
+
             var cookie = HttpContext.Current.Request.Cookies["cart"];
 
             var context = new ShopContext();
@@ -42,7 +61,7 @@ namespace Shop.Web
             {
                 var cart = new Cart() { Signature = Guid.NewGuid() };
                 HttpContext.Current.Response.Cookies.Add(new HttpCookie("cart", cart.Signature.ToString()));
-                
+
 
                 context.Carts.Add(cart);
                 context.SaveChanges();
@@ -51,23 +70,24 @@ namespace Shop.Web
             {
                 var signature = Guid.Parse(cookie.Value);
                 var cart = context.Carts.FirstOrDefault(q => q.Signature == signature);
-                
-                if(cart != null)
+
+                if (cart != null)
                 {
-                   
+
                 }
                 else
                 {
                     cart = new Cart() { Signature = Guid.NewGuid() };
                     HttpContext.Current.Response.Cookies.Add(new HttpCookie("cart", cart.Signature.ToString()));
-                    
 
                     context.Carts.Add(cart);
                     context.SaveChanges();
-
                 }
             }
+
+
         }
+
 
     }
 }
