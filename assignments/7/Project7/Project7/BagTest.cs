d8d632e314abc82f25c62ee6bd23353e1332801e
Colby-Burke <burkecl@g.cofc.edu>
2017-03-09 11:29:36 -0500

    Testing and bag/pocket

---
+++ assignments/7/Project7/Project7/BagTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDrivenDesignLecture;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class BagTest
+    {
+        [TestClass()]
+        public class BagTests
+        {
+            [TestMethod]
+            public void Bag_Should_Add_Content()
+            {
+                var bag = new Bag(2, 2, 2);
+                var content = new Content(1, 1, 1);
+                var added = bag.Add(content);
+
+                Assert.IsTrue(added);
+            }
+            [TestMethod]
+            public void Bag_Should_Not_Oversize_Content()
+            {
+                var bag = new Bag(13, 17, 23);
+                var content = new Content(20, 20, 20);
+                var added = bag.Add(content);
+
+                Assert.IsFalse(added);
+                Assert.IsFalse(bag.Check(content));
+            }
+            [TestMethod]
+            public void Bag_Should_Not_Be_Too_Small()
+            {
+                var bag = new Bag(1, 1, 1);
+                var volvume = new Volume(0, 1, 0);
+                var content = new Content(1, 2, 1);
+                var added = bag.Add(content);
+
+                Assert.IsFalse(added);
+                Assert.IsFalse(bag.Check(content));
+            }
+            [TestMethod]
+            public void One_Bag_Should_Exist_For_Contents()
+            {
+                var bag = new Bag(0, 0, 0);
+                var volume = new Volume(3, 3, 3);
+                var content = new Content(2, 2, 2);
+                var added = bag.Add(content);
+
+                Assert.IsFalse(added);
+                Assert.IsFalse(bag.Check(content));
+
+            }
+            [TestMethod]
+            public void Bag_Should_Have_Volume()
+            {
+                var bag = new Bag(2, 2, 2);
+                var volume = new Volume(1, 1, 1);
+                var content = new Content(1, 1, 1);
+                var added = bag.Add(content);
+
+                Assert.IsTrue(added);
+
+            }
+            [TestMethod]
+            public void Bag_Can_Overflow()
+            {
+                var bag = new Bag(3, 3, 3);
+                var volume = new Volume(4, 4, 4);
+                var content = new Content(1, 1, 1);
+                var added = bag.Add(content);
+
+                Assert.IsTrue(added);
+            }
+            [TestMethod]
+            public void Bag_Can_Contain_Nothing()
+            {
+                var bag = new Bag(3, 3, 3);
+                var volume = new Volume(0, 0, 0);
+                var content = new Content(0, 0, 0);
+                var added = bag.Add(content);
+
+                Assert.IsTrue(added);
+            }
+            [TestMethod]
+            public void Bag_Can_Be_Too_Big()
+            {
+                var bag = new Bag(3, 3, 3);
+                var volume = new Volume(20, 20, 20);
+                var content = new Content(1, 1, 1);
+                var added = bag.Add(content);
+
+                Assert.IsTrue(added);
+            }
+            [TestMethod]
+            public void Bag_Should_Have_Pockets()
+            {
+                var bag = new Bag(3, 3, 3);
+                var volume = new Volume(20, 20, 20);
+                var content = new Content(1, 1, 1);
+                var pocket = new Pocket(2, 1, 1);
+                var added = bag.Add(content);
+
+                Assert.IsTrue(added);
+                Assert.IsTrue(bag.Check(content));
+            }
+        }
+
+    }
+
+}
---
+++ assignments/7/Project7/Project7/PocketTest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDrivenDesignLecture;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class PocketTest
+    {
+        [TestClass()]
+        public class PocketTests
+        {
+            [TestMethod]
+            public void Pocket_Should_Add_Content()
+            {
+                var pocket = new Pocket(2, 2, 2);
+                var content = new Content(1, 1, 1);
+                var added = pocket.Add(content);
+
+                Assert.IsTrue(added);
+            }
+            [TestMethod]
+            public void Pocket_Should_Not_Oversize_Content()
+            {
+                var pocket = new Pocket(13, 17, 23);
+                var content = new Content(20, 20, 20);
+                var added = pocket.Add(content);
+
+                Assert.IsFalse(added);
+                Assert.IsFalse(pocket.Check(content));
+            }
+            [TestMethod]
+            public void Pocket_Should_Not_Be_Too_Small()
+            {
+                var pocket = new Pocket(1, 1, 1);
+                var content = new Content(1, 2, 1);
+                var added = pocket.Add(content);
+
+                Assert.IsFalse(added);
+                Assert.IsFalse(pocket.Check(content));
+            }
+            [TestMethod]
+            public void One_Pocket_Should_Exist_For_Contents()
+            {
+                var pocket = new Pocket(0, 0, 0);
+                var content = new Content(2, 2, 2);
+                var added = pocket.Add(content);
+
+                Assert.IsFalse(added);
+                Assert.IsFalse(pocket.Check(content));
+
+            }
+            [TestMethod]
+            public void Pocket_Should_Have_Volume()
+            {
+                var pocket = new Pocket(2, 2, 2);
+                var volume = new Volume(1, 1, 1);
+                var content = new Content(1, 1, 1);
+                var added = pocket.Add(content);
+
+                Assert.IsTrue(added);
+
+            }
+            [TestMethod]
+            public void Pocket_Can_Overflow()
+            {
+                var pocket = new Pocket(3, 3, 3);
+                var volume = new Volume(4, 4, 4);
+                var content = new Content(1, 1, 1);
+                var added = pocket.Add(content);
+
+                Assert.IsTrue(added);
+            }
+            [TestMethod]
+            public void Pocket_Can_Contain_Nothing()
+            {
+                var pocket = new Pocket(3, 3, 3);
+                var volume = new Volume(0, 0, 0);
+                var content = new Content(0, 0, 0);
+                var added = pocket.Add(content);
+
+                Assert.IsTrue(added);
+            }
+            [TestMethod]
+            public void Bag_Can_Be_Too_Big()
+            {
+                var bag = new Bag(3, 3, 3);
+                var volume = new Volume(20, 20, 20);
+                var content = new Content(1, 1, 1);
+                var added = bag.Add(content);
+
+                Assert.IsTrue(added);
+            }
+            [TestMethod]
+            public void pocket_Should_Be_On_A_Bag()
+            {
+                var bag = new Bag(3, 3, 3);
+                var volume = new Volume(20, 20, 20);
+                var content = new Content(1, 1, 1);
+                var pocket = new Pocket(2, 2, 2);
+                var added = bag.Add(content);
+
+                Assert.IsTrue(added);
+                Assert.IsTrue(bag.Check(content));
+            }
+
+        }
+
+    }
+
+}
