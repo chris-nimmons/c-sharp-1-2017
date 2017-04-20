dbdf942059d656f3642776855590931a344d511a
Colby-Burke <burkecl@g.cofc.edu>
2017-04-20 15:24:11 -0400

    Updated image dummy data

--- assignments/8/Shop/Shop.Models/Migrations/Configuration.cs
+++ assignments/8/Shop/Shop.Models/Migrations/Configuration.cs
@@ -20,11 +20,12 @@ namespace Shop.Web.Models
         {
             if (!context.Products.Any())
             {
-                var product = new Product() { Name = "Nerf  Gun", Price = 49, Quantity = 7, SKU = "00000", Weight = 10 };
+
+                var product = new Product() { Name = "Nerf  Gun", Price = 49, Quantity = 7, SKU = "00000", Weight = 10, Image = "http://d2rormqr1qwzpz.cloudfront.net/photos/2012/05/29/38134-maverick.jpg" };
                 context.Products.Add(product);
-                product = new Product() { Name = "Computer Monitor", Price = 10, Quantity = 20, SKU = "00110", Weight = 3 };
+                product = new Product() { Name = "Computer Monitor", Price = 10, Quantity = 20, SKU = "00110", Weight = 3, Image = "https://www.bhphotovideo.com/images/images2500x2500/acer_um_fg6aa_b01_gn246hl_bbid_24_3d_ready_1073477.jpg" };
                 context.Products.Add(product);
-                product = new Product() { Name = "Computer Mouse", Price = 5, Quantity = 3, SKU = "11110", Weight = 1 };
+                product = new Product() { Name = "Computer Mouse", Price = 5, Quantity = 3, SKU = "11110", Weight = 1, Image = "https://i.kinja-img.com/gawker-media/image/upload/s--Jo7sinAv--/c_scale,f_auto,fl_progressive,q_80,w_800/aoj4ajmkg11pq7jdwkm5.jpg" };
                 context.Products.Add(product);
 
                 context.SaveChanges();
